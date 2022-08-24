de11ef4d11d671840f7b606eb2a703117e8873d0
Simion <simion.okola@student.moringaschool.com>
2022-08-25 02:14:01 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -2,9 +2,46 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
 
+let hrtstatus = {
+  [EMPTY_HEART]: FULL_HEART,
+  [FULL_HEART]: EMPTY_HEART
+};
 
+//Color def: Red when cliced and vice versa
+const Colors = {
+  '': 'red',
+  'red': ''
+};
+
+                                                   
+document.addEventListener('DOMContentLoaded', () => {    // Grab the clicks and add event listener after the page loads
+  const like = document.querySelectorAll('.like-glyph');
+  for (const btn of like){                         //loop over all grabbed like buttons
+    btn.addEventListener('click', likePost);
+  }
+})
+
+function likePost(event){
+  const hrt = event.target; //targeting the element containing like hearts
+
+  mimicServerCall()  // Invoking mimicServerCall() upon click and server messagee alert
+  .then( (serverMessage) => {
+    alert(serverMessage); 
+
+    
+    hrt.innerText = hrtstatus[hrt.innerText]; // color changes
+    hrt.style.color = Colors[hrt.style.color]; 
+  })
+  .catch( (error) => {
+    document.getElementById('modal').removeAttribute('hidden'); 
+    let errorElem = document.getElementById('modal-message'); //garbbing error element and outputing error from the server
+    errorElem.textContent = error.message; 
+    setTimeout( () => { 
+      document.getElementById('modal').hidden = true
+    }, 3000);
+  })
+}
 
 
 //------------------------------------------------------------------------------
